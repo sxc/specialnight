697fa41f4dd8fd4d3de39b4da837517af2bec25b
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-11 16:22:08 +0800

    add database

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+
+	"github.com/sxc/specialnight/types"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sxc/specialnight/api"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "specialnight"
+const userColl = "users"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "John",
+		LastName:  "Appleseed",
+	}
+
+	_, err = coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(res)
+	var john types.User
+	if err := coll.FindOne(ctx, bson.M{}).Decode(&john); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(john)
+
 	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
 	flag.Parse()
 
